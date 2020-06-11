1bdfb66f074c94806711aa803e564e219faabf61
VinodPreet <vinodp99@yahoo.com>
2020-06-11 17:53:57 -0400

    adding db changes

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
@@ -10,6 +17,19 @@ express()
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool',(req,res)=>res.send(cool()))
   .get('/times',(req,res)=> res.send(showTimes()))
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
   showTimes = () => {
